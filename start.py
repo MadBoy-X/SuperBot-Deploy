a2be5b4cb8de18a1f698e4e1dc30465e76b3a4cc
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-11 13:28:53 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,3 @@
+from os import system
+system ("git clone https://GitHub.com/MadBoy-X/SuperBot && cd SuperBot && python3 -m SuperBot")
+print ("The SuperBot has crashed, save the logs and share it in @SuperBot_Support for help.")
