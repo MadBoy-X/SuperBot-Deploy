ad395f3c5e7e3c8e74e8a9cf887fd9eb32c8ae05
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-05 09:46:55 +0530

    Failed

--- start.py
+++
@@ -1,3 +0,0 @@
-from os import system
-system ("git clone https://GitHub.com/MadBoy-X/SuperBot && cd SuperBot && python3 -m SuperBot")
-print ("The SuperBot has crashed, save the logs and share it in @SuperBot_Support for help.")
